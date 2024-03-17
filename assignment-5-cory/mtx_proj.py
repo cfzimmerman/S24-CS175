b4703d423c5a2b96b581664c891ede4c13396415
cfzimmerman <coryzimmerman93@gmail.com>
2024-03-17 00:15:24 -0400

    ps5: rough initial pset draft

--- assignment-5-cory/mtx_proj.py
+++ assignment-5-cory/mtx_proj.py
@@ -40,22 +40,14 @@ proj = np.array([
     [0, 0, -1, 0]
 ])
 
-q = np.array([
-    [3, 0, 0, 0],
-    [0, 3, 0, 0],
-    [0, 0, 3, 0],
-    [0, 0, 0, 1]
-])
-
-s = np.array([
-    [3, 0, 0, 0],
-    [0, 3, 0, 0],
-    [0, 0, 3, 0],
-    [0, 0, 0, 3]
-])
-
 
 def q11_2():
+    q = np.array([
+        [3, 0, 0, 0],
+        [0, 3, 0, 0],
+        [0, 0, 3, 0],
+        [0, 0, 0, 1]
+    ])
     print(np.matmul(proj, q))
     for eye_coords in get_eye_coords():
         clip_no_q = get_clip(proj, eye_coords)
@@ -66,6 +58,12 @@ def q11_2():
 
 
 def q11_3():
+    s = np.array([
+        [3, 0, 0, 0],
+        [0, 3, 0, 0],
+        [0, 0, 3, 0],
+        [0, 0, 0, 3]
+    ])
     for eye_coords in get_eye_coords():
         clip_no_s = get_clip(proj, eye_coords)
         clip_w_s = get_clip(np.matmul(proj, s), eye_coords)
@@ -75,6 +73,12 @@ def q11_3():
 
 
 def q11_4():
+    q = np.array([
+        [3, 0, 0, 0],
+        [0, 3, 0, 0],
+        [0, 0, 3, 0],
+        [0, 0, 0, 1]
+    ])
     for eye_coords in get_eye_coords():
         clip_no_q = get_clip(proj, eye_coords)
         clip_w_q = get_clip(np.matmul(q, proj), eye_coords)
@@ -83,7 +87,61 @@ def q11_4():
         print("ndc q:\n", clip_to_ndc(clip_w_q))
 
 
+def get_viewport(w: float, h: float) -> np.array:
+    return np.array([
+        [w / 2, 0, 0, (w - 1) / 2],
+        [0, h / 2, 0, (h - 1) / 2],
+        [0, 0, 0.5, 0.5],
+        [0, 0, 0, 1]
+    ])
+
+
+def to_vector(mtx: np.array):
+    print(mtx.transpose().tolist()[0])
+
+
+def q12_3():
+    width = 512
+    height = 256
+    # Note that this q is different from the previous.
+    # Names follow those in the assignment
+    q = np.array([
+        [3, 0, 0, 0],
+        [0, 3, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+    view = get_viewport(width, height)
+    q_view = np.matmul(q, view)
+
+    for eye_coords in get_eye_coords():
+        ndc = clip_to_ndc(get_clip(proj, eye_coords))
+        print("original window: ",
+              np.matmul(view, ndc)
+                .transpose()
+                .tolist())
+        print("q window: ",
+              np.matmul(q_view, ndc)
+              .transpose()
+              .tolist())
+
+
+def ps6():
+    width = 512
+    height = 512
+    view = get_viewport(width, height)
+    ndc = np.array([
+        [-0.1],
+        [0.26],
+        [0],
+        [1]
+    ])
+    print(to_vector(np.matmul(view, ndc)))
+
+
 if __name__ == "__main__":
     # q11_2()
     # q11_3()
-    q11_4()
+    # q11_4()
+    # q12_3()
+    ps6()
